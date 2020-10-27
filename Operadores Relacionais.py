304aacada0d1f286d824fa89888e773cf3c4403b
Sandro Funk <sagfunk@gmail.com>
2020-10-27 19:36:19 -0300

    Add files via upload

---
+++ Operadores Relacionais.py
@@ -0,0 +1,14 @@
+# Operadores Relacionais
+# Neste programa mostro os principais 
+# operadores relacionais e suas comparações 
+# utilizando as variáveis X e Y
+
+x = 2
+y = 3
+
+print(x == y) # igual
+print(x < y)  # menor
+print(x > y)  # maior
+print(x >= y) # maior ou igual
+print(x <= y) # menor ou igual
+print(x != y) # diferente
